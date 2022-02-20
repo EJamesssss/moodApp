8a694d9d75b532b08ee94a0f5169383438aa38a7
EJamesssss <ejames.hegina@gmail.com>
2022-02-20 18:26:38 +0800

    mood app structure

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 var currentTime = new Date()
-var timeNow = currentTime.getHours() + ":" + currentTime.getMinutes()
+var hh = currentTime.getHours()
+var mm = currentTime.getMinutes()
+if(mm < 10){
+    mm = '0'+mm
+}
+
+var timeNow = hh+ ":" + mm
 
 var displayTime = document.getElementById('currentTime')
 var greet = document.getElementById('greeting')
+var uName = document.querySelector('#userName')
+var greetUser = uName.textContent = 'Elton'
 displayTime.textContent = timeNow
 
-if(timeNow > 12){
-    greet.textContent = 'Good Morning'
-}else if(timeNow == 12){
-    greet.textContent = 'Have your Lunch'
-}else if(timeNow > 12 || timeNow < 18 ){
-    greet.textContent = 'Good Afternoon'
-}else if(timeNow < 18){
-    greet.textContent = 'Good Evening'
+if(hh < 12 && 0 < mm <= 59){
+    greet.textContent = 'Good Morning, ' + greetUser
+    document.getElementById('contents').style.backgroundImage = 'url("images/bg1.jpg")'
+}else if(hh < 18 && 0 < mm <= 59){
+    greet.textContent = 'Good Afternoon, ' + greetUser
+    document.getElementById('contents').style.backgroundImage = 'url("images/afternoon.jpg")'
+    document.getElementById('contents').style.color = 'black'
+    
+}else if(hh >= 18 && 0 < mm <= 59){
+    greet.textContent = 'Good Evening, ' + greetUser
+    document.getElementById('contents').style.backgroundImage = 'url("images/night.jpg")'
+    document.getElementById('contents').style.color = 'white'
 }
