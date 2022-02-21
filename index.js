2d73064d1d37e607a6e11cdbc2012d698be23269
EJamesssss <ejames.hegina@gmail.com>
2022-02-21 20:56:28 +0800

    monday revisions

--- index.js
+++ index.js
@@ -10,19 +10,39 @@ var timeNow = hh+ ":" + mm
 var displayTime = document.getElementById('currentTime')
 var greet = document.getElementById('greeting')
 var uName = document.querySelector('#userName')
-var greetUser = uName.textContent = 'Elton'
+// var greetUser = uName.textContent = 'Elton'
 displayTime.textContent = timeNow
 
-if(hh < 12 && 0 < mm <= 59){
-    greet.textContent = 'Good Morning, ' + greetUser
-    document.getElementById('contents').style.backgroundImage = 'url("images/bg1.jpg")'
-}else if(hh < 18 && 0 < mm <= 59){
-    greet.textContent = 'Good Afternoon, ' + greetUser
-    document.getElementById('contents').style.backgroundImage = 'url("images/afternoon.jpg")'
-    document.getElementById('contents').style.color = 'black'
-    
-}else if(hh >= 18 && 0 < mm <= 59){
-    greet.textContent = 'Good Evening, ' + greetUser
-    document.getElementById('contents').style.backgroundImage = 'url("images/night.jpg")'
-    document.getElementById('contents').style.color = 'white'
+function getUserName(){
+    var text
+    var visitorName = prompt("Please enter your name:")
+    if (visitorName == null || visitorName ==""){
+        text = "Unknown"
+    }else{
+        text = visitorName
+    }
+    return text
+}
+
+function showTime(){
+    if(hh < 12 && 0 < mm <= 59){
+        greet.textContent = 'Good Morning, ' + getUserName()
+        document.getElementById('contents').style.backgroundImage = 'url("images/bg1.jpg")'
+    }else if(hh < 18 && 0 < mm <= 59){
+        greet.textContent = 'Good Afternoon, ' + getUserName()
+        document.getElementById('contents').style.backgroundImage = 'url("images/afternoon.jpg")'
+        document.getElementById('contents').style.color = 'black'
+        
+    }else if(hh >= 18 && 0 < mm <= 59){
+        greet.textContent = 'Good Evening, ' + getUserName()
+        document.getElementById('contents').style.backgroundImage = 'url("images/night.jpg")'
+        document.getElementById('contents').style.color = 'white'
+    }
+}
+
+showTime()
+
+function AssignFocus() {
+    var enterFocus = prompt("Enter your focus:")
+    document.getElementById("writeFocus").innerHTML = enterFocus
 }
